7f92a586b15a8edf2e67314c9861adc03040751a
anikeshk <anikesh.kamath@gmail.com>
2024-08-30 14:34:34 +0530

    Added city field in users model

--- server/src/models/users.models.ts
+++ server/src/models/users.models.ts
@@ -6,6 +6,7 @@ const UserSchema = new Schema(
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
+    city: { type: String, required: true },
   },
   { timestamps: true }
 );
