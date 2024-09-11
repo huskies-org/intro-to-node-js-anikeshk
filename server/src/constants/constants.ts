f5d9cb92de29b37124270dc9f2d2ee2ebf3221b2
Anikesh Kamath <anikesh.kamath@gmail.com>
2024-09-11 12:06:53 -0400

    Update constants.ts

--- server/src/constants/constants.ts
+++ server/src/constants/constants.ts
@@ -2,6 +2,7 @@ export enum ErrorCodes {
   DB_ERROR = 599,
   INVALID_INPUT = 600,
   USERNAME_EXISTS = 601,
+  TEMP = 700,
 }
 
 export const ValidPaths: string[] = ['/health', '/users/register', '/users/login', '/tasks'];
